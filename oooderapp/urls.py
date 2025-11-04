db92cd213f28c15e15e843c4eae4f8012564e449
fujino <fko2447032@stu.o-hara.ac.jp>
2025-11-04 14:47:36 +0900

    gamense

--- oooderapp/urls.py
+++ oooderapp/urls.py
@@ -1,8 +1,17 @@
-from djanogo.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
+
 
 app_name = 'oooderapp'
 
-url_patterns = [
+urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
+    path('menu/', views.MenuView.as_view(), name='menu'),
+    path('cart/', views.cartView.as_view(), name='cart'),
+    path('history/', views.historyView.as_view(), name='history'),
+    path('order/', views.orderView.as_view(), name='order'),
+    path('pay/', views.payView.as_view(), name='pay'),
+    path('welcome/', views.welcomeView.as_view(), name='welcome'),
 ]
--- oooderapp/views.py
+++ oooderapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.urls import path
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+# ここから下画面遷移のためのView
+# 変更(削除)予定
+class MenuView(TemplateView):
+    template_name = 'menu.html'
+
+class cartView(TemplateView):
+    template_name = 'cart.html'
+
+class historyView(TemplateView):
+    template_name = 'history.html'
+
+class orderView(TemplateView):
+    template_name = 'order.html'
+
+class payView(TemplateView):
+    template_name = 'pay.html'
+
+class welcomeView(TemplateView):
+    template_name = 'welcome.html'
+
+# ここまで画面遷移のためのView
