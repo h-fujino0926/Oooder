b59fba645b465795050cb70deb6abe6a9c8f457a
fujino <fko2447032@stu.o-hara.ac.jp>
2025-11-07 11:49:05 +0900

    accounts

--- Oooder/urls.py
+++ Oooder/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('oooderapp.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 
 ]
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,19 @@
-# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-# from .models import CustomUser
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
 
-# class CustomUserCreationForm(UserCreationForm):
-#     model = CustomUser
-#     fields = 
+class CustomUserCreationForm(UserCreationForm):
+    user_id = forms.CharField(label='ユーザーID', max_length=30, required=True)
+    email = forms.EmailField(label='メールアドレス',required=True)
+    name = forms.CharField(label="名前", max_length=30, required=False)
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('user_id','password1','password2', 'email', 'name')
+        labels = {
+            'user_id': 'ユーザーID',
+            'password1': 'パスワード',
+            'password2': 'パスワード（確認用）',
+            'email': 'メールアドレス',
+            'name': '名前',
+        }
+        help_texts = {"username": None,}
