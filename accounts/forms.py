6d2be2c258c118449c6dc15637a564198785b5a3
fujino <fko2447032@stu.o-hara.ac.jp>
2025-11-13 12:23:32 +0900

    fujino db

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,17 +3,6 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    user_id = forms.CharField(label='ユーザーID', max_length=30, required=True)
-    email = forms.EmailField(label='メールアドレス',required=True)
-    name = forms.CharField(label="名前", max_length=30, required=False)
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = CustomUser
-        fields = ('user_id','password1','password2', 'email', 'name')
-        labels = {
-            'user_id': 'ユーザーID',
-            'password1': 'パスワード',
-            'password2': 'パスワード（確認用）',
-            'email': 'メールアドレス',
-            'name': '名前',
-        }
-        help_texts = {"username": None,}
+        fields = ('username', 'email', 'password1', 'password2')
